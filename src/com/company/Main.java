0f0c950f1fab81259869e50da531922f293cd3b9
monnetek <boun@monnetek.com>
2016-12-05 16:52:41 -0600

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,56 @@
+package com.company;
+
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+    public static User user;
+
+    public static void main(String[] args) {
+        ArrayList<Message> messages = new ArrayList<>();
+
+        Spark.init();
+
+        Spark.get(
+                "/",
+                ((request, response) -> {
+                    HashMap m = new HashMap();
+                    if(user == null) {
+                        return new ModelAndView(m, "index.html");
+                    } else {
+                        m.put("name", user.name);
+                        m.put("messages", messages);
+                        return new ModelAndView(m, "messages.html");
+                    }
+                }),
+                new MustacheTemplateEngine()
+        );
+
+        Spark.post(
+                "/create-user",
+                ((request, response) -> {
+                    String name = request.queryParams("loginName");
+                    user = new User(name);
+                    response.redirect("/");
+                    return "";
+                })
+        );
+
+        Spark.post(
+
+                "/create-message",
+                ((request, response) -> {
+                    String text = request.queryParams("userMessage");
+                    Message p = new Message (text);
+                    messages.add(p);
+                    response.redirect("/");
+                    return "";
+                })
+        );
+    }
+}
---
+++ src/com/company/User.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/**
+ * Created by boun on 12/5/16.
+ */
+public class User {
+    String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+
+}
